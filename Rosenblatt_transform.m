e7f54235f100743d04f2294708d3ea676d2ffc9d
Iago Pereira Lemos <lemosiago123@gmail.com>
2020-06-19 10:35:08 -0300

    Update Rosenblatt_transform.m

--- Rosenblatt_transform.m
+++ Rosenblatt_transform.m
@@ -1,9 +1,30 @@
 function [obj_normal] = Rosenblatt_transform(obj_dist, x)
 %% Rosenblatt transformation - Nonnormal distribution to normal distribution
 %{
+MIT License
+
+Copyright (c) 2020 Iago Pereira Lemos
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 ---------------------------------------------------------------------------
 Created by: Iago Pereira Lemos (lemosiago123@gmail.com)
-Federal University of Uberl‚ndia, School of mechanical engineering
+Federal University of Uberl√¢ndia, School of mechanical engineering
 ---------------------------------------------------------------------------
 Inputs
 obj_dist: Distribution object nonnormal made with makedist or fitdist
